20837723e2a098a284c6bb59bb37cb9ed600bd92
Forest <767425412lin@gmail.com>
2022-11-24 21:58:17 +0800

    feat: add variables

---
+++ 02 variable.py
@@ -0,0 +1,10 @@
+name = 'forest'
+nickname = name = 'changlin'
+print('name:', name)  # changlin
+print('nickname:', nickname)  # changlin
+
+
+x, y, z = 1, 2, 3
+print('x:', x)
+print('y:', y)
+print('z:', z)
